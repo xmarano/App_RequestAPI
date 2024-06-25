82a25b7ec4bf4ef0ec1d05cb925005eb0635f6ee
xmarano <leo.gregori@epitech.eu>
2024-06-25 13:00:26 +0200

    Initial Commit

---
+++ RequestAPI/RequestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RequestAPIApp.swift
+//  RequestAPI
+//
+//  Created by Léo Grégori on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RequestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
